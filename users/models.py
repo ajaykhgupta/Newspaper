dbe1aac89ef7c6c610b6009f7e2e0756dd8420b5
Ajay K Gupta <ajay.k.gupta@innovaccer.com>
2021-12-15 20:10:58 +0530

    added pages app & authentication

---
+++ users/models.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+class CustomUser(AbstractUser):
+    id = models.BigAutoField(primary_key=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
